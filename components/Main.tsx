8107df073a8ee54ef5d7e8f52b7f7350d937989f
gregroyclark <gregroyclark@gmail.com>
2023-04-12 19:42:27 -0700

    updated content

--- components/Main.tsx
+++ components/Main.tsx
@@ -8,9 +8,23 @@ const Main = (props: Props) => {
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
           <p className='uppercase text-sm tracking-widest text-gray-600'>
-            Solving your creative problems
+            bring your ideas to life
           </p>
           <h1 className='py-4 text-gray-700'>Clark Creative Services</h1>
+          <p>
+            In the internet age, your business doesn&apos;t exist if it
+            isn&apos;t on the web.
+            <br />
+            Your customers need to be able to find you.
+            <br />
+            They need to understand who you are & what you do.
+            <br />
+            Most importantly, it has to be easy.
+            <br />
+            Modern attention spans can&apos;t handle friction.
+            <br />
+            I&apos;m here to solve those problems for you.
+          </p>
         </div>
       </div>
     </div>
