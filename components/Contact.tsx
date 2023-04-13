d112bef39d443128f7ef5d7d8f41b654e5357076
gregroyclark <gregroyclark@gmail.com>
2023-04-13 14:06:33 -0700

    reduced amount of rows

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -92,7 +92,7 @@ const Contact = (props: Props) => {
                   <label className='uppercase text-sm py-2'>Message</label>
                   <textarea
                     className='border-2 rounded-lg p-3 border-gray-300'
-                    rows={10}
+                    rows={5}
                   />
                 </div>
                 <button className='w-full p-4 text-gray-100 mt-4'>
