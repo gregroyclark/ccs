0c72b8bb2b09652e96827f8440bac438ee54b253
gregroyclark <gregroyclark@gmail.com>
2023-04-12 18:23:34 -0700

    added logo to mobile nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,56 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-        <Image
-          src='/../public/logo.png'
-          alt='Clark Creative Services blue logo'
-          width={75}
-          height={125}
-          className='bg-[#ecf0f3]'
-        />
-        <p>this is the navbar</p>
+        <Link href={'/'}>
+          <Image
+            src='/../public/logo.png'
+            alt='Clark Creative Services blue text logo'
+            width={75}
+            height={125}
+            className='bg-[#ecf0f3]'
+          />
+        </Link>
+
+        <div>
+          <ul className='hidden md:flex'>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href={'/'}>Home</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href={'/services'}>Services</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href={'/work'}>Work</Link>
+            </li>
+            <li className='ml-10 text-sm uppercase hover:border-b'>
+              <Link href={'/contact'}>Contact</Link>
+            </li>
+          </ul>
+          <div className='md:hidden'>
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+      <div className='fixed left-0 top-0 w-full h-screen bg-black/30'>
+        <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-white p-10 ease-in duration-300'>
+          <div>
+            <div>
+              <Image
+                src={'/../public/logo.png'}
+                alt='Clark Creative Services blue text logo'
+                width={87}
+                height={35}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
