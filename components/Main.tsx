5b12af322fa8c29823977bfc72893cba84770f3a
gregroyclark <gregroyclark@gmail.com>
2023-04-12 20:33:04 -0700

    trimmed ts boilerplate

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-type Props = {};
-
-const Main = (props: Props) => {
+const Main = () => {
   return (
     <div className='w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
