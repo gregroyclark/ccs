a1931c1e26c9f90fe0b2c0ba62c30be5a2b6dfe8
gregroyclark <gregroyclark@gmail.com>
2023-04-12 22:28:28 -0700

    added Portfolio

---
+++ components/Portfolio.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type Props = {};
+
+const Portfolio = (props: Props) => {
+  return (
+    <div id='portfolio' className='w-full'>
+      <div className='max-w-[1240px] mx-auto px-2 py-16'>
+        <p>Portfolio</p>
+        <h2>Some of my finest work</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
