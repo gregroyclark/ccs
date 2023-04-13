db4a2325364a52cc2c5cf16625e664412ba5e872
gregroyclark <gregroyclark@gmail.com>
2023-04-12 22:13:26 -0700

    added events

--- components/Services.tsx
+++ components/Services.tsx
@@ -3,17 +3,16 @@ import React from 'react';
 
 import design from '../public/services/design.png';
 import development from '../public/services/development.png';
+import event from '../public/services/event.png';
 import writing from '../public/services/writing.png';
 import search from '../public/services/search.png';
 import social from '../public/services/social.png';
 import marketing from '../public/services/marketing.png';
 
-type Props = {};
-
-const Services = (props: Props) => {
+const Services = () => {
   return (
     <div id='services' className='w-full lg:h-screen p-2'>
-      <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
+      <div className='max-w-[1240px] mx-auto flex flex-col justify-center sm:h-full'>
         <p className='text-xl tracking-widest uppercase'>Services</p>
         <h2></h2>
         <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8 p-4'>
@@ -84,6 +83,21 @@ const Services = (props: Props) => {
               </div>
               <div className='flex flex-col items-center justify-center'>
                 <h3>Marketing</h3>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div className='m-auto'>
+                <Image
+                  src={event}
+                  alt={'Event Coordination'}
+                  width={64}
+                  height={64}
+                />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>Events</h3>
               </div>
             </div>
           </div>
